6fecf9d5f03d704c9aef04f531776cc383ab5ac8
Marco Burgarello <marco.burgarello@yahoo.com>
2018-06-30 16:12:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form').on('submit', function(g) {
+  $('form').('submit', function(e) {
     alert('Your form is going to be submitted now.')
   })
 }
